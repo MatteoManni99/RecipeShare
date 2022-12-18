c6d275b9bcb6e4801698e9d3159b3cc98a6d7cf8
MatteoManni99 <81640598+MatteoManni99@users.noreply.github.com>
2022-12-18 22:19:49 +0100

    Update App.java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,6 +4,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World 43" );
+        System.out.println( "Hello World 3" );
+        System.out.println( "Hello World 10" );
     }
 }
