5457e8ae6e82d2c84698f86f62d4d143c7ac4296
Loremazze <mazzeilorenzo35@virgilio.it>
2023-01-08 21:02:12 +0100

    Login

--- src/main/java/com/example/demo1/LoginController.java
+++ src/main/java/com/example/demo1/LoginController.java
@@ -11,6 +11,7 @@ import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import org.bson.Document;
 import org.bson.conversions.Bson;
@@ -29,22 +30,14 @@ public class LoginController {
 
     @FXML
     public void onLogoutClick(ActionEvent actionEvent) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-        Scene scene = new Scene(fxmlLoader.load(), 500, 500);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+        String nomeSchermata = "hello-view.fxml";
+        cambiaSchermata(actionEvent,nomeSchermata);
     }
 
     @FXML
     public void onRegisterClick(ActionEvent actionEvent) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Register.fxml"));
-        stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-        Scene scene = new Scene(fxmlLoader.load(), 500, 500);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+        String nomeSchermata = "Register.fxml";
+        cambiaSchermata(actionEvent,nomeSchermata);
     }
 
     public void onLoginClick(ActionEvent actionEvent) {
@@ -52,30 +45,52 @@ public class LoginController {
             NAME = Carmen
             PASSWORD = 6czYhW4F
         */
+        String nomePagina = null;
         String name = insertedName.getText();
         String password = insertedPassword.getText();
         String uri = "mongodb://localhost:27017";
         try (MongoClient mongoClient = MongoClients.create(uri)) {
-            MongoDatabase database = mongoClient.getDatabase("RecipeShare"); //da scegliere il nome uguale per tutti
-            MongoCollection<Document> collection = database.getCollection("author");
-            Bson filter = Filters.and(
+            MongoDatabase database = mongoClient.getDatabase("test"); //da scegliere il nome uguale per tutti
+            MongoCollection<Document> collectionAuthor = database.getCollection("author");
+            MongoCollection<Document> collectionModerator = database.getCollection("moderator");
+            Bson filterAuthor = Filters.and(
                     Filters.eq("authorName", name),
                     Filters.eq("password", password));
-            MongoCursor<Document> cursor = collection.find(filter).iterator();
-            if (cursor.hasNext()) {
-                System.out.println("TROVATO");
-                //cambio pagina
-                FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Loggato.fxml"));
-                stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
-                Scene scene = new Scene(fxmlLoader.load(), 600, 500);
-                stage.setTitle("Hello "+ name);
-                stage.setScene(scene);
-                stage.show();
+            MongoCursor<Document> cursorAuthor = collectionAuthor.find(filterAuthor).iterator();
+            Bson filterModerator = Filters.and(
+                    Filters.eq("moderatorName", name),
+                    Filters.eq("password", password));
+            MongoCursor<Document> cursorModerator = collectionModerator.find(filterModerator).iterator();
+            if (cursorAuthor.hasNext()) {
+                System.out.println("TROVATO AUTHOR");
+                nomePagina = "Loggato.fxml";
             }
-            else System.out.println("N");
+            else {
+                System.out.println("NON TROVATO AUTHOR");
+                if (cursorModerator.hasNext()) {
+                    System.out.println("TROVATO MODERATOR");
+                    nomePagina = "Moderator.fxml";
+                }
+                else System.out.println("NON TROVATO MODERATOR");
+            }
+            if (nomePagina != null) {
+                cambiaSchermata(actionEvent,nomePagina);
+            }
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public void cambiaSchermata(ActionEvent actionEvent,String nomeSchermata) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource(nomeSchermata));
+        stage = (Stage) ((Node)actionEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene(fxmlLoader.load(), 600, 500);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void onTextClick(MouseEvent mouseEvent) {
+        System.out.println("TESTO CLICKATO");
     }
 }
