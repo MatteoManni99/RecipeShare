3b1414a26a0535cfe1ca59f87bfe914e0dd27c4f
Leonardo <leonardo.bargiotti@stud.unifi.it>
2023-01-10 15:24:10 +0100

    Schermata Recipe

---
+++ src/main/java/com/example/demo1/RecipeController.java
@@ -0,0 +1,13 @@
+package com.example.demo1;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+public class RecipeController {
+    private Stage stage;
+
+    private ImageView imageView;
+    private String url; // URL IMAGE
+
+    private Image image = new Image(url);
+}
