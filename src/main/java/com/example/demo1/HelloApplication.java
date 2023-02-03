d158de452e7b5596970c699a069b7d23d6558f9d
MatteoManni99 <matteomanni1999@gmail.com>
2023-02-03 10:49:00 +0100

    test repl

--- src/main/java/com/example/demo1/HelloApplication.java
+++ src/main/java/com/example/demo1/HelloApplication.java
@@ -1,16 +1,33 @@
 package com.example.demo1;
 
 import com.example.demo1.dao.mongo.ReportedRecipeMongoDAO;
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ReadPreference;
+import com.mongodb.WriteConcern;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.bson.Document;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        MongoClientSettings mcs = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString("mongodb://10.1.1.8:27019"))
+                .readPreference(ReadPreference.nearest())
+                .retryWrites(true)
+                .writeConcern(WriteConcern.W1).build();
+        MongoClient client = MongoClients.create(mcs);
+        System.out.println(client.getDatabase("RecipeShare").getCollection("author")
+                .find(new Document("authorName","Malarkey Test")).first().getString("authorName"));
+
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1000, 600);
         stage.setResizable(false);
